665cb66a511ec0da759a93bd52444598464e35d1
@Shrii 🤍✨ <172282120+KellumXz@users.noreply.github.com>
2025-07-23 23:04:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,15 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
+DELETE_MESSAGES:"private",
+OWNER_NUMBER:"94755084612",
+READ_MESSAGE: "true",
+AUTO_STATUS_SEEN: "true",
+AUTO_STATUS_REACT: "true",
+ONE_VIEW:"on",
+AUTO_STATUS_REPLY:"ture",
+AUTO_STATUS_MSG:"*Seen Now*",
+PASSWORD:"Kalum@2008",
 SESSION_ID: process.env.SESSION_ID || "KELUMXZ=v1Jy3BJT#jVY-VsgOh23JF4IMyw7prtkqF5wC8k_Ll9eIgfRmr3M",
 CREATE_NUMBER:"94766927934"
 };
